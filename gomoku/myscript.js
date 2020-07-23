862243a8930596de1152ab3f037dcc34dac711b8
jherndon8 <jherndon8@gatech.edu>
2020-07-23 09:08:03 -0500

    New link

--- gomoku/myscript.js
+++ gomoku/myscript.js
@@ -5,8 +5,7 @@ function getPostData(postID, next) {
        return; 
     }
     var url = "https://share.jodel.com/post/"
-    document.getElementById("postlink").innerHTML="<iframe frameborder=\"0\" height=\"200px\" width=\"400px\" src=\"https://share.jodel.com/post/preview?postId=" + postID + "\" style=\"border-radius:3px\"></iframe>";
-    document.getElementById("postlink").innerHTML='<iframe frameborder="0" height="300px" width="600px" src="https://share.jodel.com/post/preview?postId=' + postID + '" style="border-radius:3px"></iframe>'
+    document.getElementById("postlink").innerHTML='<iframe frameborder="0" height="400px" width="600px" src="https://share.jodel.com/post/preview?postId=' + postID + '" style="border-radius:3px"></iframe>'
     var url = url + postID + "/replies";
     if (next) {
         url = url + "?next=" + next
